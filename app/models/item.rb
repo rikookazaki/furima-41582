8e471696dc70812ac35c935441c70e04d67645c9
rikookazaki <riko.35.oka@gmail.com>
2024-10-07 19:38:18 +0900

    アソシエーション定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :condition
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :postage
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :how_long
+
+  belongs_to :user
 end

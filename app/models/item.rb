00ebefc88d91e774c8ba77ea63c0efe486522007
rikookazaki <riko.35.oka@gmail.com>
2024-10-08 17:36:05 +0900

    Itemモデル単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,7 +13,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :image, :name, :explanation, :user,
+  validates :name, :explanation, :user_id,
             presence: true
   validates :price, presence: true, format: { with: /\A[0-9]+\z/ },
                     numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    name              { Faker::Name }
+    explanation       { Faker::Lorem.sentence }
+    price             { Faker::Number.between(from: 300, to: 99_999_99) }
+    category_id       { Faker::Number.between(from: 2, to: 11) }
+    condition_id      { Faker::Number.between(from: 2, to: 7) }
+    postage_id        { Faker::Number.between(from: 2, to: 3) }
+    prefecture_id     { Faker::Number.between(from: 2, to: 48) }
+    how_long_id       { Faker::Number.between(from: 2, to: 4) }
+
+    association :user
   end
 end

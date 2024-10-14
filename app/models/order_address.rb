f2d9ba966f9df705cd309f6576eeba8da4d9657d
rikookazaki <riko.35.oka@gmail.com>
2024-10-14 14:44:33 +0900

    OrderAddressクラス記述

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,2 +1,10 @@
 class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :post_cord, :prefecture_id, :city, :address_num, :building, :phone_num, :order_id, :user_id, :item_id
+
+  with_options presence: true do
+    validates :user_id, :item_id, :order_id, :prefecture_id, :city, :address_num
+    validates :post_cord, format: { with: /\A\d{3}-\d{4}\z/ }
+    validates :phone_num, format: { with: /\A\d{10,11}\z/ }
+  end
 end

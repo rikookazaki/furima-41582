6a0eee82c6c8006ce8e7c9b8b19032634d00b4d3
rikookazaki <riko.35.oka@gmail.com>
2024-10-14 17:21:25 +0900

    ユーザー遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,7 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+  before_action :move_to_index, only: [:index]
+  before_action :check_sold_out, only: [:index]
   def index
     @order_address = OrderAddress.new
     @item = Item.find(params[:item_id])
@@ -21,5 +24,19 @@ class OrdersController < ApplicationController
     params.require(:order_address).permit(:post_cord, :prefecture_id, :city, :address_num, :building, :phone_num).merge(
       user_id: current_user.id, item_id: params[:item_id]
     )
+  end
+
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    return unless current_user.id == @item.user_id
+
+    redirect_to root_path
+  end
+
+  def check_sold_out
+    @item = Item.find(params[:item_id])
+    return unless @item.order.present?
+
+    redirect_to root_path
   end
 end

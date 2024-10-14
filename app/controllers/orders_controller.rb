1577fa3c888a6eb685f79577ed6d43d01b88052b
rikookazaki <riko.35.oka@gmail.com>
2024-10-14 15:43:56 +0900

    ordersコントローラー記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,24 @@
 class OrdersController < ApplicationController
+  def index
+    @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_address_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def order_address_params
+    params.require(:order_address).permit(:post_cord, :prefecture_id, :city, :address_num, :building, :phone_num).merge(
+      user_id: current_user.id, item_id: params[:item_id]
+    )
+  end
 end

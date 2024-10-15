f2be379c7940059cbeb5668d124ef303ff6a1d9c
rikookazaki <riko.35.oka@gmail.com>
2024-10-15 14:44:35 +0900

    before_action修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
-  before_action :find_item, only: [:show, :edit, :update, :destroy, :move_to_index, :check_sold_out]
+  before_action :find_item, only: [:show, :edit, :update, :destroy, :move_to_index]
   before_action :move_to_index, only: [:edit, :destroy]
   before_action :check_sold_out, only: [:edit]
   def index
